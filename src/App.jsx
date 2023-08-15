dcd5f45901870529d1c475fc81ae801eda2cbeb5
Abolfazl <abolfazlmarzban39@gmail.com>
2023-08-15 09:14:50 +0430

    setting up the layout

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,11 @@ const App = () => {
     <div className="w-full py-16 px-32">
       <div className='flex items-center'>
         <img src={check} alt="" className='w-10 h-10' />
-        <h1 className='pl-3 text-4xl'>Task List</h1>
+        <h1 className='pl-3 text-4xl font-bold'>Task List</h1>
       </div>
-
+      <p className='mt-14 font-medium'>Break your life to simple tasks to get things done!
+      <br></br>
+Does not matter how many tasks you done, It’s important to break to small tasks and be on progress.</p>
     </div>
 
   );
